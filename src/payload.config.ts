d831d3fe7bcdc218f5824fd528c6c4f537dcfa8d
Nikola Ganchev <nikolamihailovg@gmail.com>
2023-05-02 14:45:45 +0300

    Add uploading to GCS in production environments

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -3,6 +3,16 @@ import path from 'path';
 import Users from './collections/Users';
 import News from "./collections/News";
 import Media from "./collections/Media";
+import { cloudStorage } from "@payloadcms/plugin-cloud-storage";
+import { gcsAdapter } from "@payloadcms/plugin-cloud-storage/gcs";
+
+const adapter = gcsAdapter({
+  options: {
+    apiEndpoint: process.env.GCS_ENDPOINT,
+    projectId: process.env.GCS_PROJECT_ID,
+  },
+  bucket: process.env.GCS_BUCKET,
+})
 
 export default buildConfig({
   admin: {
@@ -19,4 +29,15 @@ export default buildConfig({
   graphQL: {
     schemaOutputFile: path.resolve(__dirname, 'generated-schema.graphql'),
   },
+  plugins: [
+    // Only use cloud storage in prod
+    process.env.NODE_ENV == 'production' ? cloudStorage({
+      collections: {
+        'media': {
+          adapter,
+          disableLocalStorage: true
+        },
+      },
+    }) : null,
+  ].filter(value => value !== null),
 });
