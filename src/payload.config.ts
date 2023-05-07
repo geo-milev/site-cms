ce0bdd873aa6ad98bce78a25122ec2f8fe402102
Nikola Ganchev <nikolamihailovg@gmail.com>
2023-05-07 19:56:40 +0300

    Replace footer in favor of contacts global

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -10,7 +10,7 @@ import MainInfo from "./globals/MainInfo";
 import Slideshow from "./globals/Slideshow";
 import VideoSection from "./globals/VideoSection";
 import WhatIsStudied from "./globals/WhatIsStudied";
-import Footer from "./globals/Footer";
+import Contact from "./globals/Contact";
 
 const adapter = gcsAdapter({
   options: {
@@ -31,7 +31,7 @@ export default buildConfig({
     Media
   ],
   globals: [
-    MainInfo, Slideshow, VideoSection, WhatIsStudied, Footer
+    MainInfo, Slideshow, VideoSection, WhatIsStudied, Contact
   ],
   typescript: {
     outputFile: path.resolve(__dirname, 'payload-types.ts'),
